3d2a8201e750668a1a243ed761d05b840b6731e1
Almaz Kunpeissov <almaz.kun@gmail.com>
2020-05-12 13:12:47 +0900

    heroku DBadded

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,13 @@ TEMPLATES = [
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
