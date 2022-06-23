d93db76692796df1956c5d49594ef301d42cc626
Almaz Kunpeissov <almaz.kun@gmail.com>
2022-06-23 18:51:54 +0900

    complete rewrite

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-
-from .views import api_receiver
-
-
-urlpatterns = [path("", api_receiver, name="api_receiver")]
--- pages/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-
-from .models import UTM_source
-
-
-admin.site.register(UTM_source)
